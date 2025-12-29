e5c96efd01fa13a15bac1296360bcc10c0ee955f
Pedro Eduardo <pedroeduardodeoliveira@gmail.com>
2025-12-29 15:41:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -283,8 +283,9 @@ app.post("/api/send-test", async (req, res) => {
 // SERVIDOR
 // =========================
 
-const PORT = process.env.PORT || 3112;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`🌐 Rodando em http://localhost:${PORT}`);
 });
+
